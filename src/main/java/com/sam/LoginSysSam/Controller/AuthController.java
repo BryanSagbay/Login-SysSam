7e04f2e06e4d5c977b0f0f447478fbde3610677b
BryanSagbay <bryansagbay2001@hotmail.com>
2024-05-23 15:17:36 -0500

    Add swagger

--- src/main/java/com/sam/LoginSysSam/Controller/AuthController.java
+++ src/main/java/com/sam/LoginSysSam/Controller/AuthController.java
@@ -6,14 +6,12 @@ import com.sam.LoginSysSam.Auth.RegisterRequest;
 import com.sam.LoginSysSam.Service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final AuthService authService;
